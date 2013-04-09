8b6d87818591b7d99c06c984b6837d5c376c9abe
S Tonkin <sptonkin@outlook.com>
2013-04-09 22:32:42 +1000

    first cut of setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+import re
+import platform
+import os
+import sys
+
+def load_version(filename='restq/version.py'):
+    """Parse a __version__ number from a source file"""
+    with open(filename) as source:
+        text = source.read()
+        match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", text)
+        if not match:
+            msg = "Unable to find version number in {}".format(filename)
+            raise RuntimeError(msg)
+        version = match.group(1)
+        return version
+
+setup(
+    ame="restq",
+    version=load_version(),
+    packages=['restq'],
+    package_data=dict(restq=restq_package_data),
+    data_files=data_files,
+    zip_safe=False,
+    author="Michael Dorman",
+    author_email="mjdorma@gmail.com",
+    url="",
+    description="All-in-memory job queue with RESTful interface.",
+    long_description=open('README.rst').read(),
+    license="Apache Software Licence",
+    install_requires = ["bottle>=0.11"],
+    platforms=['cygwin', 'win', 'linux'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Security',
+        'Topic :: System :: Monitoring'
+    ],
+    test_suite="tests"
+)
