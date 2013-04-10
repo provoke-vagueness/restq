38b9ce4f76ecc19ae49a1e5d2a4fe1354f232f01
mjdorma <mjdorma+github@gmail.com>
2013-04-11 01:11:55 +1000

    bjoern server

--- restq/__main__.py
+++ restq/__main__.py
@@ -3,9 +3,15 @@ import bottle
 import webapp
 import realms 
 
+try:
+    import bjoern
+    server="bjoern"
+except ImportError:
+    server=None
 
 if __name__ == "__main__":
     bottle.run(app=webapp.app, 
+                server=server,
                 host='localhost', 
                 port=8080, 
                 debug=True)
