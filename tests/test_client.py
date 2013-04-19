6d2f69627bc0081953d4ac5173c99beb6876ca76
mjdorma <mjdorma+github@gmail.com>
2013-04-19 19:37:30 +1000

    extends test_realms general test but hooks the requester object with a webtest test app

---
+++ tests/test_client.py
@@ -0,0 +1,73 @@
+import unittest
+from pprint import pprint
+import time
+import os
+
+import webtest
+
+import test_realms
+from restq import webapp
+from restq import client
+
+
+
+def hooker(func):
+    def decorate(*a, **k):
+        class Res(object):
+            def __init__(self, res):
+                self._res = res
+
+            def __getattribute__(self, k):
+                res = object.__getattribute__(self, '_res')
+                if k == 'json':
+                    return lambda: res.json
+                return getattr(res, k)
+        data = k.pop('data', None)
+        if data is not None:
+            a = list(a)
+            a.append(data)
+        return Res(func(*a, **k))
+    return decorate
+
+
+class Requester():
+    """Make sure the webtest.TestApp behaves like the "requests" library."""
+
+    def __init__(self):
+        self.app = webtest.TestApp(webapp.app)
+
+    @hooker
+    def put(self, *a, **k):
+        return self.app.put(*a, **k)
+
+    @hooker
+    def delete(self, *a, **k):
+        return self.app.delete(*a, **k)
+
+    @hooker
+    def post(self, *a, **k): 
+        return self.app.post(*a, **k)
+
+    @hooker
+    def get(self, *a, **k): 
+        return self.app.get(*a, **k)
+
+
+
+class TestClient(test_realms.TestJobs):
+
+    def setUp(self):
+        test_realms.TestJobs.setUp(self)
+        requester = Requester()
+        self.realms = client.Realms(url='', requester=requester)
+    
+
+
+
+
+
+
+
+
+
+
