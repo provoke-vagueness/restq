7c2e850f9e6f3c3395e74c6c9b152a0c557afd95
mick <mick@mick-ubuntu.casa-matina>
2013-04-26 15:46:24 +1000

    now able to get a job or realm using getitem...  data is not mandatory

---
+++ tests/load_hash_test.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+import os
+import hashlib
+
+import restq
+
+
+
+
+
+
+
+def do_test(url='http://localhost:8080'):
+    realms = restq.Realms(url=url)
+    test = realms.test
+    for i in range(100000):
+        job_id = hashlib.md5(str(i)).hexdigest()
+        test.add(job_id, 0)
+
+    print(len(test.pull(count=100000)))
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    do_test()
+
+
+
+
+
+
+
+
+
+
