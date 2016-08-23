191c0aee125f4a4726d1645e8e531441a6cc34c4
ACSC CyberLab <cyber.lab@defence.gov.au>
2016-08-23 11:41:51 +1000

    Replace custom queue iter (that can fail under some conditions) with simpler implementation

--- restq/realms.py
+++ restq/realms.py
@@ -17,35 +17,6 @@ else:
     iternext = lambda i: i.__next__()
 
 
-# Need a circular (ring) iterator to walk the ordered dict and we also need to
-# be able to revert a yielded object in some circumstances...  This facility
-# makes it possible to keep our queue order and fairness
-class QueueIterator:
-    def __init__(self, queue):
-        self._queue = queue
-        self._count = 0
-        self._iter = dictiter(self._queue)
-
-    def next(self):
-        assert self._queue
-        while True:
-            try:
-                obj = next(self._iter)
-                self._count += 1
-                break
-            except StopIteration:
-                self._iter = dictiter(self._queue)
-        return obj
-
-    def revert(self):
-        self._count -= 1
-        self._iter = dictiter(self._queue)
-        index = len(self._queue) % self._count
-        while index > 0:
-            next(self._iter)
-            index -= 1
-
-
 # Serialise access to functions of Jobs
 def serialise(func):
     @wraps(func)
@@ -64,7 +35,6 @@ class Realm:
     def __init__(self, realm_id):
         self.realm_id = realm_id
         self.queues = {}
-        self.queue_iter = {}
         self.queue_lease_time = {}
         self.default_lease_time = config.realms['default_lease_time']
         self.tags = {}
@@ -92,6 +62,9 @@ class Realm:
             for queue_id in job[JOB_QUEUES]:
                 queue = self.queues[queue_id]
                 queue.pop(job_id)
+            # recreate dict to release mem from large allocations
+            if not len(queue):
+                self.queues[queue_id] = OrderedDict()
 
     @serialise
     def remove_job(self, job_id):
@@ -226,33 +199,17 @@ class Realm:
         queues_ids = [k for k in self.queues]
         queues_ids.sort()
         jobs = {}
+        ctime = time.time()
         for queue_id in queues_ids:
-            #skip queues that have no jobs
-            if not self.queues[queue_id]:
-                continue
+            # add this job to the jobs result dict and update its lease time
+            for job_id, dequeue_time in dictiter(self.queues[queue_id]):
 
-            #get our queue iterator
-            iterator = self.queue_iter[queue_id]
-
-            #pull a max of count jobs from the queue
-            while True:
-                job_id, dequeue_time = iterator.next()
-
-                # add this job to the jobs result dict and update its lease time
-                ctime = time.time()
+                # skip any non expired leases
                 if ctime - dequeue_time > self.queue_lease_time[queue_id]:
                     self.queues[queue_id][job_id] = ctime
                     jobs[job_id] = (queue_id, self.jobs[job_id][JOB_DATA])
                     if len(jobs) >= count:
                         return jobs
-                else:
-                    #iff the queues maintain insert order and the iteration
-                    # sequence walks the queue on that insert order we can
-                    #rely on the lease time being consistent such that all jobs
-                    #following this point will also met this same condition
-                    # -> go to next queue
-                    iterator.revert()
-                    break
         return jobs
 
     @serialise
@@ -339,7 +296,6 @@ class Realm:
     def _create_queue(self, queue_id, lease_time):
         queue = OrderedDict()
         self.queues[queue_id] = queue
-        self.queue_iter[queue_id] = QueueIterator(queue)
         self.queue_lease_time[queue_id] = lease_time
         return queue
 
